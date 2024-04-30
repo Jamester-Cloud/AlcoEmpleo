32be7953609203d48deaf5f977d9080537de3d74
Angel Mendez <110472083+Enyel04@users.noreply.github.com>
2024-04-30 12:40:50 -0400

    Agregando Listado de Empleos

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react"
-import Header from "../components/Headers/Header";
+import Header from "../components/Headers/header";
 import Footer from "../components/Footer/footer";
+
 
 export default function layout({
     children,
@@ -10,7 +11,7 @@ export default function layout({
     return (
         <div className="">
             <Header />
-
+      
     
             {children}
             <Footer/>
