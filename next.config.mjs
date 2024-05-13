03038fdab606b10353eada566683db2d6f683ca2
Ivan Rodriguez <rodriang62@gmail.com>
2024-05-13 16:19:12 -0400

    Cambio en la configuracion de next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    eslint:false,
     typescript: {
         ignoreBuildErrors: true
-    }
+    },
+    
 };
 
 export default nextConfig;
