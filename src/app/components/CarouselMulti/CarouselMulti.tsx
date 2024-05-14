0204567f1f99043dc30f525cb01441be0bb88019
Angel Mendez <110472083+Enyel04@users.noreply.github.com>
2024-05-14 10:21:12 -0400

    Arreglado Carousel CandidatosPremiums

--- src/app/components/CarouselMulti/CarouselMulti.tsx
+++ src/app/components/CarouselMulti/CarouselMulti.tsx
@@ -1,19 +1,18 @@
-"use client"
+"use client";
 import React from "react";
 import { Card } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocation } from "@fortawesome/free-solid-svg-icons";
 import { Candidate } from "@/app/interfaces/types";
 import Carousel from "react-multi-carousel";
-
+import "react-multi-carousel/lib/styles.css";
 import Link from "next/link";
 
-//Se llama al Candidate[] Premium
 interface Props {
   candidates: Candidate[];
 }
 
-export default function CandidateCarousel({ candidates }: Props) {
+export function CarouselMulti({ candidates }: Props) {
   return (
     <Carousel
       responsive={{
@@ -35,9 +34,13 @@ export default function CandidateCarousel({ candidates }: Props) {
       infinite={true}
     >
       {candidates.map((candidato, index) => (
-        
         <div key={index} className="flex justify-center p-2">
-              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.css" integrity="sha256-NAxhqDvtY0l4xn+YVa6WjAcmd94NNfttjNsDmNatFVc=" crossOrigin="anonymous" />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.css"
+            integrity="sha256-NAxhqDvtY0l4xn+YVa6WjAcmd94NNfttjNsDmNatFVc="
+            crossOrigin="anonymous"
+          />
           <Card className="w-80 h-64">
             <Card.Header>
               <div className="mt-3 mb-4">
@@ -50,23 +53,22 @@ export default function CandidateCarousel({ candidates }: Props) {
               </span>
               <h4 className="mb-2">{candidato.personaData.nombre} {candidato.personaData.apellido}</h4>
               <p className="text-muted mb-4">{candidato.Candidato.perfil.puestoDeseado}</p>
-
               <div className="text-muted mb-4">
-                <FontAwesomeIcon icon={faLocation} />
-                Venezuela
+                <FontAwesomeIcon icon={faLocation} /> Venezuela
               </div>
               <div className="mb-4 pb-2">
-             
-              <button type="button" className="btn btn-success btn-sm rounded">
-                       <Link href={`https://wa.me/${candidato.personaData.telefono}?text=Hola%20te%20estamos%20contactando%20desde%20La%20web%20AlcoEmpleo%20y%20estamos%20Interesados%20en%20tu%20perfil`} className="mdi mdi-whatsapp  text-white text-decoration-none">  Enviar mensaje
-                       </Link>
-                   </button>
-               
+                <button type="button" className="btn btn-success btn-sm rounded">
+                  <Link
+                    href={`https://wa.me/${candidato.personaData.telefono}?text=Hola%20te%20estamos%20contactando%20desde%20La%20web%20AlcoEmpleo%20y%20estamos%20Interesados%20en%20tu%20perfil`}
+                    className="mdi mdi-whatsapp  text-white text-decoration-none"
+                  >
+                    Enviar mensaje
+                  </Link>
+                </button>
               </div>
-           
               <Link href={`/enterprise/candidateProfile/${candidato._id}`} className="btn btn-primary btn-large">
-                        Ver Perfil
-                    </Link>
+                Ver Perfil
+              </Link>
             </Card.Body>
           </Card>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,27 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Header from "./components/Headers/header";
 import Footer from "./components/Footer/footer";
 import Spinner from "./components/Spinner/Spinner";
 import { Card, Carousel } from "react-bootstrap";
-
 import Link from "next/link";
 import Image from "next/image";
-import CandidateCarousel from "./components/CarouselMulti/CarouselMulti";
 import { CandidateListResponse } from "./interfaces/types";
-
+import { CarouselMulti } from "./components/CarouselMulti/CarouselMulti";
 
 export default function Home() {
-  //En el State se llama el ListResponse para declarar que los datos seran llamados como Array
   const [data, setData] = useState<CandidateListResponse>({ dataCandidatosPremium: [] });
   const [loading, setLoading] = useState<boolean>(true);
 
-  
   useEffect(() => {
-    //Realizo la consulta con el axio, se llama el candidate Response
-    //Nota se puede hacer console log, para ver los objetos que trae esta data
     const fetchData = async () => {
       try {
         const response = await axios.get<CandidateListResponse>("api/enterprise/candidateList");
         setData(response.data);
         setLoading(false);
       } catch (error) {
-        console.error("Error al Cargar los datos:", error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -40,59 +34,51 @@ export default function Home() {
 
   return (
     <div>
-      <Header />
+   
       <Carousel className="">
-          <Carousel.Item className="h-64 md:h-96 lg:h-128 xl:h-144 ">
-            <div className="w-full h-full ">
-                <Image
-                  src="/slider/slider1.jpg"
-                  layout="fill"
-                  objectFit="cover"
-                  alt="GrupoAlco"
-                />
-        
-            </div>
-            <Carousel.Caption>
-              <h3 className="z-5">Texto del Slider 1</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item className="h-64 md:h-96 lg:h-128 xl:h-144 ">
-          
-              <div className="w-full h-full ">
-                <Image
-                  src="/slider/slider2.jpg"
-                  layout="fill"
-                  objectFit="cover"
-                  alt="GrupoAlco"
-                />
-              </div>
-          
-            <Carousel.Caption>
-              <h3>Texto del Slider 2</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item className="h-64 md:h-96 lg:h-128 xl:h-144">
-            
-              <div className="w-full h-full">
-                <Image
-                  src="/slider/slider3.jpg"
-                  layout="fill"
-                  objectFit="cover"
-                  alt="GrupoAlco"
-                />
-              </div>
-          
-            <Carousel.Caption>
-              <h3>Texto del Slider 3</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-    {/* Carousel de Arriba, son las imagenes, falta modificar para Agregar Textos/}
-
-      {/* Llamando al componente CandidateCarousel Premium*/}
-      <CandidateCarousel candidates={data.dataCandidatosPremium} />
-
-      <Footer />
+        <Carousel.Item className="h-64 md:h-96 lg:h-128 xl:h-144 ">
+          <div className="w-full h-full ">
+            <Image
+              src="/slider/slider1.jpg"
+              layout="fill"
+              objectFit="cover"
+              alt="GrupoAlco"
+            />
+          </div>
+          <Carousel.Caption>
+            <h3 className="z-5">Texto del Slider 1</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item className="h-64 md:h-96 lg:h-128 xl:h-144 ">
+          <div className="w-full h-full ">
+            <Image
+              src="/slider/slider2.jpg"
+              layout="fill"
+              objectFit="cover"
+              alt="GrupoAlco"
+            />
+          </div>
+          <Carousel.Caption>
+            <h3>Texto del Slider 2</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item className="h-64 md:h-96 lg:h-128 xl:h-144">
+          <div className="w-full h-full">
+            <Image
+              src="/slider/slider3.jpg"
+              layout="fill"
+              objectFit="cover"
+              alt="GrupoAlco"
+            />
+          </div>
+          <Carousel.Caption>
+            <h3>Texto del Slider 3</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      {/* Usamos el componente CandidateCarousel */}
+      <CarouselMulti candidates={data.dataCandidatosPremium} />
+    
     </div>
   );
 }
