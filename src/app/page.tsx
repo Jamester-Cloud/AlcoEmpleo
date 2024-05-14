4c6cee7dc7f49f2f1af0260da3274da81f51215c
Ivan Rodriguez <rodriang62@gmail.com>
2024-05-14 09:53:31 -0400

    cambiando a cabecera nueva

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 "use client"
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Header from "./components/Headers/header";
+import Footer from "./components/Footer/footer";
 import Spinner from "./components/Spinner/Spinner";
 import { Card, Carousel } from "react-bootstrap";
+
+import Link from "next/link";
 import Image from "next/image";
 import CandidateCarousel from "./components/CarouselMulti/CarouselMulti";
 import { CandidateListResponse } from "./interfaces/types";
+import Cabecera from "./components/cabeceras/cabecera";
 
 
 export default function Home() {
@@ -13,7 +18,7 @@ export default function Home() {
   const [data, setData] = useState<CandidateListResponse>({ dataCandidatosPremium: [] });
   const [loading, setLoading] = useState<boolean>(true);
 
-
+  
   useEffect(() => {
     //Realizo la consulta con el axio, se llama el candidate Response
     //Nota se puede hacer console log, para ver los objetos que trae esta data
@@ -36,56 +41,59 @@ export default function Home() {
 
   return (
     <div>
+      <Cabecera />
       <Carousel className="">
-        <Carousel.Item className="h-64 md:h-96 lg:h-128 xl:h-144 ">
-          <div className="w-full h-full ">
-            <Image
-              src="/slider/slider1.jpg"
-              layout="fill"
-              objectFit="cover"
-              alt="GrupoAlco"
-            />
-
-          </div>
-          <Carousel.Caption>
-            <h3 className="z-5">Texto del Slider 1</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item className="h-64 md:h-96 lg:h-128 xl:h-144 ">
-
-          <div className="w-full h-full ">
-            <Image
-              src="/slider/slider2.jpg"
-              layout="fill"
-              objectFit="cover"
-              alt="GrupoAlco"
-            />
-          </div>
-
-          <Carousel.Caption>
-            <h3>Texto del Slider 2</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item className="h-64 md:h-96 lg:h-128 xl:h-144">
-
-          <div className="w-full h-full">
-            <Image
-              src="/slider/slider3.jpg"
-              layout="fill"
-              objectFit="cover"
-              alt="GrupoAlco"
-            />
-          </div>
-
-          <Carousel.Caption>
-            <h3>Texto del Slider 3</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-      {/* Carousel de Arriba, son las imagenes, falta modificar para Agregar Textos/}
+          <Carousel.Item className="h-64 md:h-96 lg:h-128 xl:h-144 ">
+            <div className="w-full h-full ">
+                <Image
+                  src="/slider/slider1.jpg"
+                  layout="fill"
+                  objectFit="cover"
+                  alt="GrupoAlco"
+                />
+        
+            </div>
+            <Carousel.Caption>
+              <h3 className="z-5">Texto del Slider 1</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item className="h-64 md:h-96 lg:h-128 xl:h-144 ">
+          
+              <div className="w-full h-full ">
+                <Image
+                  src="/slider/slider2.jpg"
+                  layout="fill"
+                  objectFit="cover"
+                  alt="GrupoAlco"
+                />
+              </div>
+          
+            <Carousel.Caption>
+              <h3>Texto del Slider 2</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item className="h-64 md:h-96 lg:h-128 xl:h-144">
+            
+              <div className="w-full h-full">
+                <Image
+                  src="/slider/slider3.jpg"
+                  layout="fill"
+                  objectFit="cover"
+                  alt="GrupoAlco"
+                />
+              </div>
+          
+            <Carousel.Caption>
+              <h3>Texto del Slider 3</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+    {/* Carousel de Arriba, son las imagenes, falta modificar para Agregar Textos/}
 
       {/* Llamando al componente CandidateCarousel Premium*/}
       <CandidateCarousel candidates={data.dataCandidatosPremium} />
+
+      <Footer />
     </div>
   );
 }
