a0cdedbf5bc455d8a019b25efa6d2f79d9902fe4
Angel Mendez <110472083+Enyel04@users.noreply.github.com>
2024-07-02 15:11:37 -0400

    Fix: Arreglado HomePage Modelo

--- src/models/homepageModel.js
+++ src/models/homepageModel.js
@@ -1,17 +1,41 @@
 import mongoose from 'mongoose'
 
 const homepageSchema = new mongoose.Schema({
-    tituloHomePage: {
+    celular: [
+        {  numero: String}
+    ],
+    direccion: {
         type: String,
-        required: [true, "El titulo No puede estar Vacio"]
     },
-    textoHomePage: {
-        type: String,
-        required: [true, "El texto no puede estar vacio"]
-    }
-   
+    politicaPrivacidad:{
+        type:String,
+        required: [true, "Por Favor Agregue la Politica y Privacidad"]
+    },
+    sliders: [{
+        titulo: {
+          type: String,
+          required: [true, 'El título no puede estar vacío']
+        },
+        texto: {
+          type: String,
+          required: [true, 'El texto no puede estar vacío']
+        }
+      }],
+      banner: [{
+        titulo: {
+          type: String,
+          required: [true, 'El título no puede estar vacío']
+        },
+        texto: {
+          type: String,
+          required: [true, 'El texto no puede estar vacío']
+        }
+      }]
+
+
 })
 
 const homepage = mongoose.models.homepageSchema || mongoose.model("homepage", homepageSchema)
 
 export default homepage;
+
