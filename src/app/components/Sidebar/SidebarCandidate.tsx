462279dd5177613edb8ce66bec99ca03fab4ea18
Alexander Gomez <alexandergomezinfante@gmail.com>
2024-07-07 14:38:24 -0400

    sidebar en pagina de candidato

--- src/app/components/Sidebar/SidebarCandidate.tsx
+++ src/app/components/Sidebar/SidebarCandidate.tsx
@@ -1,60 +1,45 @@
 "use client";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faMagnifyingGlass,
-  faCog,
-  faCartShopping,
-  faLock,
-  faCreditCard
-} from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function SidebarCandidate({ children }: any) {
+interface SidebarProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
 
+const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
   return (
-    <div className="row flex-nowrap">
-      <div className="col-auto col-md-3 col-xl-2 px-sm-2 w-25 px-0 navbar-expand-lg bg-light" >
-        <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 min-vh-100 sticky-top">
-          {/* Iniciando Lista */}
-          <ul className="navbar   navbar-nav   flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-            <li className="nav-item ">
-              <Link href="/candidate/edit" className="nav-link d-flex align-items-center " id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                <Image src="/AlcoSloganLogo.png" alt="hugenerd" width={30} height={30} className="rounded-circle" />
-                <span className="d-none d-sm-inline mx-1">Ver Perfil</span>
-              </Link>
-            </li>
-            {/* <li>
-              <a href="#" data-bs-toggle="collapse" className="nav-link  align-middle d-flex align-items-center"  >
-                <FontAwesomeIcon icon={faMagnifyingGlass} width="30" height="30" />
-                <span className="ms-1 d-none d-sm-inline ">  Mi CV</span>
-              </a>
-            </li>*/}
-            <li>
-              <a href="#" data-bs-toggle="collapse" className="nav-link  align-middle d-flex align-items-center"  >
-                <FontAwesomeIcon icon={faCartShopping} width="30" height="30" />
-                <span className="ms-1 d-none d-sm-inline ">  Subcripción</span>
-              </a>
-            </li> {/* Final li */}
-            <li>
-              <a href="#" data-bs-toggle="collapse" className="nav-link  align-middle d-flex align-items-center"  >
-                <FontAwesomeIcon icon={faMagnifyingGlass} width="30" height="30" />
-                <span className="ms-1 d-none d-sm-inline ">  Empleos aplicados</span>
-              </a>
-            </li> {/* Final li */}
-            <li>
-            </li> {/* Final li */}
-            <li>
-            </li> {/* Final li */}
-          </ul>
-        </div>
-      </div>     {/* Todo Arriba realizado con Boostrap */}
-
-      {/* */}
-      <div className="col">
-        {children}
+    <div className={`fixed top-0 left-0 h-full bg-white shadow-lg z-50 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out`}>
+      <button onClick={onClose} className="p-4">
+        X
+      </button>
+      <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 min-vh-100 sticky-top">
+        <ul className="navbar navbar-nav flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+          <li className="nav-item">
+            <Link href="/candidate/edit" className="nav-link d-flex align-items-center" id="dropdownUser1">
+              <Image src="/AlcoSloganLogo.png" alt="hugenerd" width={30} height={30} className="rounded-circle" />
+              <span className="d-none d-sm-inline mx-1">Ver Perfil</span>
+            </Link>
+          </li>
+          <li>
+            <a href="#" className="nav-link align-middle d-flex align-items-center">
+              <FontAwesomeIcon icon={faCartShopping} width="30" height="30" />
+              <span className="ms-1 d-none d-sm-inline">Subcripción</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link align-middle d-flex align-items-center">
+              <FontAwesomeIcon icon={faMagnifyingGlass} width="30" height="30" />
+              <span className="ms-1 d-none d-sm-inline">Empleos aplicados</span>
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
-
   );
-}
+};
+
+export default Sidebar;
