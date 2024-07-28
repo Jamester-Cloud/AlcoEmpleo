6811fac69f81a0e37ce9c794f582a8b76c751109
Ivan Rodriguez <rodriang62@gmail.com>
2024-07-27 21:29:54 -0400

    Trabajando con set en el update

--- src/app/api/administrator/homepage/edit/route.tsx
+++ src/app/api/administrator/homepage/edit/route.tsx
@@ -16,16 +16,16 @@ export async function POST(request: NextRequest) {
 
         let { data: { banner, direccion, sliders, secciones, celular, politicaPrivacidad } } = reqJson;
 
-        filter = { _id: Types.ObjectId.createFromHexString('66a576701d7bc081933b2758'), idUsuarioAdministrador: Types.ObjectId.createFromHexString("669fbe254ee5de405072dfcd") }
+        filter = { _id: Types.ObjectId.createFromHexString('66a576701d7bc081933b2758') }
 
         update = {
             $set: {
-                "banner": banner,
-                "direccion": direccion,
-                "sliders": sliders,
-                "secciones": secciones,
-                "celular": celular,
-                "politicaPrivacidad": politicaPrivacidad,
+                banner: banner,
+                direccion: direccion,
+                sliders: sliders,
+                secciones: secciones,
+                celular: celular,
+                politicaPrivacidad: politicaPrivacidad,
             }
         };
         //planeo hacer el paginado aca
