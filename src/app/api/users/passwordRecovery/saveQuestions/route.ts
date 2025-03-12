285bea1773d7897788c15419243e64494e94fac5
Ivan Rodriguez <rodriang62@gmail.com>
2025-03-12 15:38:27 -0400

    added new form controoller, adding arrayField for questions

---
+++ src/app/api/users/passwordRecovery/saveQuestions/route.ts
@@ -0,0 +1,24 @@
+import { connect } from "@/dbConfig/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+
+export async function POST(request: NextRequest) {
+  try {
+    await connect();
+
+    const reqJson = await request.json();
+    let { preguntas, email } = reqJson;
+    let filter = { email: email }, update = { preguntas: preguntas };
+    await User.updateOne(filter, update);
+
+    return NextResponse.json(
+      { message: "Preguntas guardadas" },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
--- src/app/recoverPassword/page.tsx
+++ src/app/recoverPassword/page.tsx
@@ -4,10 +4,17 @@ import { useForm } from "react-hook-form";
 import { requestHandler } from "@/helpers/axiosRequest";
 import { ToastContainer, toast, Bounce } from "react-toastify";
 const RecoverPassword: React.FC = () => {
-  const [preguntas, setPreguntas] = React.useState([]);
+  const [user, setUser] = React.useState<any>({
+    preguntas: [],
+    email: "",
+    hasQuestions: false,
+  });
+
+  const [toggleForm, setToggleForm] = React.useState<boolean>(false);
+
   const { register, handleSubmit } = useForm({
     defaultValues: {
-      emailSearch:"",
+      emailSearch: "",
       question1: "",
       answer1: "",
       question2: "",
@@ -16,74 +23,170 @@ const RecoverPassword: React.FC = () => {
   });
   const onEmailSubmit = async (data: any) => {
     try {
-      const res = await requestHandler({
-        url: '/api/users/passwordRecovery/findUser/',
-        data: {email: data.emailSearch},
-      }, "post");
+      const res = await requestHandler(
+        {
+          url: "/api/users/passwordRecovery/findUser/",
+          data: { email: data.emailSearch },
+        },
+        "post"
+      );
 
-      if(res?.status == 200){
-        setPreguntas(res.data.user.preguntas);
+      if (res?.status == 200) {
+        setUser({
+          preguntas: res.data.user.preguntas,
+          email: res.data.user.email,
+          hasQuestions: res.data.user.preguntas.length > 0 ? true : false,
+        });
+        setToggleForm(true);
       }
-    } catch (error:any) {
-      console.log(error);
-      toast.error("Error:", error.message);
+    } catch (error: any) {
+      console.log(error.response.data.error);
+      let message = error.response.data.error;
+      setUser({ preguntas: [], hasQuestions: false });
+      setToggleForm(false);
+      toast.error(`Error: ${message}`);
     }
   };
+  const submitQuestions = async (data: any) => {
+    try {
+      const res = await requestHandler(
+        {
+          url: "/api/users/passwordRecovery/saveQuestions/",
+          data: { email: data.emailSearch },
+        },
+        "post"
+      );
+
+      if (res?.status == 200) {
+        setUser({
+          preguntas: res.data.user.preguntas,
+          email: res.data.user.email,
+          hasQuestions: res.data.user.preguntas.length > 0 ? true : false,
+        });
+        setToggleForm(true);
+      }
+    } catch (error: any) {
+      toast.error(`Error: ${error.response.data.error}`);
+    }
+  };
+
   return (
     <div className="container-fluid mt-5">
       <h1 className="text-center mb-4">Recuperacíon de cuenta</h1>
-      <form onSubmit={handleSubmit(onEmailSubmit)} className="card p-4 mb-5">
-        <div className="row mb-3">
-          <div className="col-8">
-          <input {...register("emailSearch", {required:"Campo obligatorio"})} type="text" className="form-control" />
+      <div className="card p-4 mb-5">
+        <form onSubmit={handleSubmit(onEmailSubmit)}>
+          <div className="row mb-3">
+            <div className="col-8">
+              <input
+                {...register("emailSearch", { required: "Campo obligatorio" })}
+                type="text"
+                className="form-control"
+              />
+            </div>
+            <div className="col-4">
+              <button
+                type="submit"
+                className="btn btn-primary btn-block btn-md"
+              >
+                Buscar
+              </button>
+            </div>
           </div>
-          <div className="col-4">
-            <button type="submit" className="btn btn-primary w-50">
-              Buscar
-            </button>
-          </div>
-        </div>
-        {/* Here */}
-        <div className="card p-4 mb-3">
-          {/* <div className="mb-3">
-            <label htmlFor="question1" className="form-label">
-              Pregunta 1
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="question1"
-              name="question1"
-              required
-            />
-          </div> */}
-          {/* <div className="mb-3">
-            <label htmlFor="question2" className="form-label">
-              Pregunta 2
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="question2"
-              name="question2"
-              required
-            />
-          </div> */}
-        </div>
-        {/* <button type="submit" className="btn btn-primary w-100">Recover Password</button> */}
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-      </form>
+          {/* <button type="submit" className="btn btn-primary w-100">Recover Password</button> */}
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
+        </form>
+
+        {toggleForm && (
+          <form onSubmit={handleSubmit(submitQuestions)}>
+            {!user.hasQuestions ? (
+              <div className="card p-4 mb-3">
+                <div className="mb-3">
+                  <label htmlFor="question1" className="form-label">
+                    Pregunta 1
+                  </label>
+                  {/* Pregunta */}
+                  <input
+                    type="text"
+                    className="form-control mb-3"
+                    id="question1"
+                    name="question1"
+                    placeholder="Ingrese una pregunta de seguridad"
+                  />
+                  {/* respuesta */}
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="question1"
+                    name="question1"
+                    placeholder="Ingrese una respuesta"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="question2" className="form-label">
+                    Pregunta 2
+                  </label>
+                  {/* Pregunta */}
+                  <input
+                    type="text"
+                    className="form-control mb-3"
+                    placeholder="Ingrese una pregunta de seguridad"
+                  />
+                  {/* respuesta */}
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="question2"
+                    name="question2"
+                    placeholder="Ingrese una respuesta"
+                  />
+                </div>
+              </div>
+            ) : (
+              <>
+                <div className="card p-4 mb-3">
+                  <div className="mb-3">
+                    <label htmlFor="question1" className="form-label">
+                      Pregunta 1
+                    </label>
+                    {/* respuesta */}
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="question1"
+                      name="question1"
+                      placeholder="Ingrese una respuesta"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="question2" className="form-label">
+                      Pregunta 2
+                    </label>
+                    {/* respuesta */}
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="question2"
+                      name="question2"
+                      placeholder="Ingrese una respuesta"
+                    />
+                  </div>
+                </div>
+              </>
+            )}
+          </form>
+        )}
+      </div>
     </div>
   );
 };
