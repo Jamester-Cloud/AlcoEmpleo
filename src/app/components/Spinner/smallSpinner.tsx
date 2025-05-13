ee654b62a17ee3cbd13d9bd86ffea56a453a05ea
Ivan Rodriguez <rodriang62@gmail.com>
2025-05-13 12:03:41 -0400

    added smallSpinner component

---
+++ src/app/components/Spinner/smallSpinner.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useState, CSSProperties, useEffect } from "react";
+import MoonLoader from "react-spinners/MoonLoader";
+
+const override: CSSProperties = {
+  margin: "0 auto",
+ 
+};
+
+export default function Spinner({loading}: {loading: boolean}) {
+  return (
+        <div className="sweet-loading">
+          <MoonLoader
+            color="#576FE3" // Simplificado el color
+            loading={loading}
+            cssOverride={override}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+            size={60}
+          />
+    </div>
+  );
+}
